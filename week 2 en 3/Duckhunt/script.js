e3cdfad7ba1a241c303de5abe4f8bbf6f90e2df5
Joey <99062903@mydavinci.nl>
2020-12-01 16:00:37 +0100

    duckhunt basic concept af

---
+++ week 2 en 3/Duckhunt/script.js
@@ -0,0 +1,50 @@
+var positionX = 500;
+var positionY = 300;
+
+var duck = document.getElementById("duck").addEventListener("click", duck);
+function duck(){
+	var duck = document.getElementById("duck").style.display="none";
+}
+
+var left = document.getElementById("left").addEventListener("click", left);
+function left(){
+	positionX -= 75;
+	console.log(positionX);	
+	var positionX2 = document.getElementById("duck").style.left = positionX + "px";
+	var positionY2 = document.getElementById("duck").style.top = positionY + "px";
+}
+
+var right = document.getElementById("right").addEventListener("click", right);
+function right(){
+	positionX += 75;
+	console.log(positionX);
+	var positionX2 = document.getElementById("duck").style.left = positionX + "px";
+	var positionY2 = document.getElementById("duck").style.top = positionY + "px";
+}
+
+var top2 = document.getElementById("top").addEventListener("click", top2);
+function top2(){
+	positionY -= 75;
+	console.log(positionY);
+	var positionX2 = document.getElementById("duck").style.left = positionX + "px";
+	var positionY2 = document.getElementById("duck").style.top = positionY + "px";
+}
+
+var bottom = document.getElementById("bottom").addEventListener("click", bottom);
+function bottom(){
+	positionY += 75;
+	console.log(positionY);
+	var positionX2 = document.getElementById("duck").style.left = positionX + "px";
+	var positionY2 = document.getElementById("duck").style.top = positionY + "px";
+}
+
+var spawn = document.getElementById("respawn").addEventListener("click", respawn);
+function respawn(){
+	positionY = 300;
+	positionX = 500;
+	var positionX2 = document.getElementById("duck").style.left = positionX + "px";
+	var positionY2 = document.getElementById("duck").style.top = positionY + "px";
+	var duck = document.getElementById("duck").style.display="inline-block";
+	console.log(positionX);	
+	console.log(positionY);
+}
